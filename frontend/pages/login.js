15cf6165559c0180f332bc895ef3ba2ea6a6bd31
boomerangth <55072391+boomerangth@users.noreply.github.com>
2022-04-25 23:53:21 +0700

    add hompage

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,12 +1,38 @@
 import Head from 'next/head'
-import Image from 'next/image'
+//import Layout from '../components/layout'
+import { useState } from 'react'
+//import Navbar from '../components/navbar'
 import styles from '../styles/Home.module.css'
+import axios from 'axios'
+import config from '../config/config'
 
-export default function login() {
+
+export default function Login({token}) {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [status, setStatus] = useState('')
+
+    const login = async (req, res) => {
+        try {
+            let result = await axios.post(`${config.URL}/login`,
+                { username, password },
+                { withCredentials: true })
+            console.log('result: ', result)
+            console.log('result.data:  ', result.data)
+            console.log('token:  ', token)
+            setStatus(result.status + ': ' + result.data.user.username)
+        }
+        catch (e) {
+            console.log('error: ', JSON.stringify(e.response))
+            setStatus(JSON.stringify(e.response).substring(0, 80) + "...")
+        }
+    }
   return (
+
+      
 <div class="w-full flex flex-wrap">
 
-
+<title>login</title>
 <div class="w-full md:w-1/2 flex flex-col">
 
     <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
@@ -18,18 +44,18 @@ export default function login() {
         <form class="flex flex-col pt-3 md:pt-8" onsubmit="event.preventDefault();">
             <div class="flex flex-col pt-4">
                 <label for="email" class="text-lg">Email</label>
-                <input type="email" id="email" placeholder="your@email.com" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input type="username" name="username" placeholder="your username" onChange={(e) => setUsername(e.target.value)} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
             </div>
 
             <div class="flex flex-col pt-4">
                 <label for="password" class="text-lg">Password</label>
-                <input type="password" id="password" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
+                <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"/>
             </div>
 
-            <input type="submit" value="Log In" class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8"/>
+            <button class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" onClick={login}>Login</button>
         </form>
         <div class="text-center pt-12 pb-12">
-            <p>Don't have an account? <a href="register" class="underline font-semibold">Register here.</a></p>
+            <p>Don't have an account? <a href="register1" class="underline font-semibold">Register here.</a></p>
         </div>
     </div>
 
@@ -42,4 +68,7 @@ export default function login() {
 </div>
 
   )
+}
+export function getServerSideProps({ req, res }) {
+    return { props: { token: req.cookies.token || "" } };
 }
--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -1,8 +1,42 @@
-export default function register() {
+import { useState } from 'react'
+import Head from 'next/head'
+//import Layout from '../components/layout'
+import styles from '../styles/Home.module.css'
+//import Navbar from '../components/navbar'
+import axios from 'axios'
+import config from '../config/config'
+export default function Register() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [status, setStatus] = useState('')
+
+    const profileUser = async () => {
+        console.log('token: ', token)
+        const users = await axios.get(`${config.URL}/profile`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+        console.log('user: ', users.data)
+    }
+
+    const register = async (req, res) => {
+        try {
+            let result = await axios.post(`${config.URL}/register`,
+                { username, email, password })
+            console.log('result: ', result)
+            console.log('result.data:  ', result.data)
+            console.log('token:  ', token)
+            setStatus(result.data.message)
+        }
+        catch (e) {
+            console.log(e)
+        }
+
+    }
     return (
         <div class="w-full flex flex-wrap">
 
-        
+        <title>register</title>
         <div class="w-full md:w-1/2 flex flex-col">
 
             <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-12">
@@ -13,26 +47,27 @@ export default function register() {
                 <p class="text-center text-3xl">Join Us.</p>
                 <form class="flex flex-col pt-3 md:pt-8" onsubmit="event.preventDefault();">
                     <div class="flex flex-col pt-4">
-                        <label for="name" class="text-lg">Name</label>
-                        <input type="text" id="name" placeholder="John Smith" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                        <label for="name" class="text-lg">Username</label>
+                        <input type="username" name="username" placeholder="username" onChange={(e) => setUsername(e.target.value)} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
                     </div>
 
                     <div class="flex flex-col pt-4">
                         <label for="email" class="text-lg">Email</label>
-                        <input type="email" id="email" placeholder="your@email.com" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                        <input type="email" name="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
                     </div>
     
                     <div class="flex flex-col pt-4">
                         <label for="password" class="text-lg">Password</label>
-                        <input type="password" id="password" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
+                        <input type="password" id="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
                     </div>
 
-                    <div class="flex flex-col pt-4">
+                    {/* <div class="flex flex-col pt-4">
                         <label for="confirm-password" class="text-lg">Confirm Password</label>
                         <input type="password" id="confirm-password" placeholder="Password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline" />
-                    </div>
+                    </div> */}
     
-                    <input type="submit" value="Register" class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" />
+                    
+                    <button class="bg-black text-white font-bold text-lg hover:bg-gray-700 p-2 mt-8" onClick={register}>Register</button>
                 </form>
                 <div class="text-center pt-12 pb-12">
                     <p>Already have an account? <a href="login" class="underline font-semibold">Log in here.</a></p>
